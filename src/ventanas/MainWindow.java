032f60d0fc510ce767e979da6ad489f5c3d0106d
JJFR2 <JJFR2@DESKTOP-JIDITIJ>
2022-04-25 14:04:32 +0200

    No habia añadido las ventanas solo actualizado las existentes.

---
+++ src/ventanas/MainWindow.java
@@ -0,0 +1,79 @@
+package ventanas;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTable;
+
+@SuppressWarnings("serial")
+public class MainWindow extends JFrame {
+
+	JTable table;
+	JPanel panel;
+	JButton details, insert, update, close, statistics;
+
+	public MainWindow() {
+		setBounds(100, 100, 600, 400);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		getContentPane().setLayout(null);
+		setLocationRelativeTo(null);
+
+		table = new JTable();
+		table.setBounds(102, 65, 369, 175);
+		getContentPane().add(table);
+
+		JLabel lblNewLabel = new JLabel("User: ");
+		lblNewLabel.setBounds(170, 30, 67, 13);
+		getContentPane().add(lblNewLabel);
+
+		JLabel lblNewLabel_1 = new JLabel("New label");
+		lblNewLabel_1.setBounds(331, 30, 92, 13);
+		getContentPane().add(lblNewLabel_1);
+
+		panel = new JPanel();
+		panel.setBounds(23, 262, 532, 38);
+		getContentPane().add(panel);
+
+		details = new JButton("Details");
+		panel.add(details);
+
+		insert = new JButton("Insert");
+		panel.add(insert);
+
+		update = new JButton("Update");
+		update.addActionListener(new ActionListener() {
+
+			@SuppressWarnings("unused")
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				Statistics_Update statistics = new Statistics_Update();
+				dispose();
+
+			}
+		});
+		panel.add(update);
+
+		close = new JButton("Close");
+		panel.add(close);
+
+		statistics = new JButton("Statistics");
+		statistics.setBounds(233, 310, 109, 21);
+		getContentPane().add(statistics);
+		statistics.addActionListener(new ActionListener() {
+
+			@SuppressWarnings("unused")
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				Statistics_Update statistics = new Statistics_Update();
+				dispose();
+
+			}
+		});
+		setVisible(true);
+	}
+
+}
---
+++ src/ventanas/Statistics_Update.java
@@ -0,0 +1,82 @@
+package ventanas;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+
+@SuppressWarnings("serial")
+public class Statistics_Update extends JFrame {
+
+	JTextField titlef, yearf, lblNewLabel_2f;
+	JButton next, cancel;
+	JLabel title, year, lblNewLabel_2;
+
+	public Statistics_Update() {
+
+		setBounds(100, 100, 450, 300);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		getContentPane().setLayout(null);
+		setLocationRelativeTo(null);
+
+		title = new JLabel("Title:");
+		title.setBounds(123, 53, 45, 13);
+		getContentPane().add(title);
+
+		year = new JLabel("Year:");
+		year.setBounds(123, 76, 45, 13);
+		getContentPane().add(year);
+
+		// No entiendo bien lo que pone en la foto de la pizarra
+		lblNewLabel_2 = new JLabel("New label");
+		lblNewLabel_2.setBounds(123, 99, 45, 13);
+		getContentPane().add(lblNewLabel_2);
+
+		next = new JButton("Continue");
+		next.setBounds(101, 163, 85, 21);
+		getContentPane().add(next);
+		next.addActionListener(new ActionListener() {
+
+			@SuppressWarnings("unused")
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				MainWindow main = new MainWindow();
+				dispose();
+			}
+		});
+
+		cancel = new JButton("Cancel");
+		cancel.setBounds(218, 163, 85, 21);
+		getContentPane().add(cancel);
+		cancel.addActionListener(new ActionListener() {
+
+			@SuppressWarnings("unused")
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				MainWindow main = new MainWindow();
+				dispose();
+			}
+		});
+
+		titlef = new JTextField();
+		titlef.setBounds(207, 50, 96, 19);
+		getContentPane().add(titlef);
+		titlef.setColumns(10);
+
+		yearf = new JTextField();
+		yearf.setBounds(207, 73, 96, 19);
+		getContentPane().add(yearf);
+		yearf.setColumns(10);
+
+		// No entiendo bien lo que pone en la foto de la pizarra
+		lblNewLabel_2f = new JTextField();
+		lblNewLabel_2f.setBounds(207, 96, 96, 19);
+		getContentPane().add(lblNewLabel_2f);
+		lblNewLabel_2f.setColumns(10);
+		setVisible(true);
+	}
+
+}
