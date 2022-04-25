c555cda3377207977ff7ff82289b3b6b73c8880d
JJFR2 <JJFR2@DESKTOP-JIDITIJ>
2022-04-25 09:57:16 +0200

    Creación parte visual de Inicio y Registro.

--- src/ventanas/InicioSesion.java
+++ src/ventanas/InicioSesion.java
@@ -4,10 +4,14 @@ import java.awt.EventQueue;
 
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JButton;
+import javax.swing.JTextField;
 
 public class InicioSesion {
 
 	private JFrame frame;
+	private JTextField textField;
+	private JTextField textField_1;
 
 	/**
 	 * Launch the application.
@@ -37,20 +41,35 @@ public class InicioSesion {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 500, 356);
+		frame.setResizable(false);
+		frame.setBounds(100, 100, 250, 181);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 		
 		JLabel lblNewLabel = new JLabel("Username:");
-		lblNewLabel.setBounds(127, 78, 55, 13);
+		lblNewLabel.setBounds(25, 32, 70, 13);
 		frame.getContentPane().add(lblNewLabel);
 		
 		JLabel lblNewLabel_1 = new JLabel("Password:");
-		lblNewLabel_1.setBounds(127, 112, 55, 13);
+		lblNewLabel_1.setBounds(25, 55, 70, 13);
 		frame.getContentPane().add(lblNewLabel_1);
 		
-		JLabel lblNewLabel_2 = new JLabel("Tlfno. :");
-		lblNewLabel_2.setBounds(127, 153, 45, 13);
-		frame.getContentPane().add(lblNewLabel_2);
+		JButton btnNewButton = new JButton("Register");
+		btnNewButton.setBounds(10, 93, 85, 21);
+		frame.getContentPane().add(btnNewButton);
+		
+		JButton btnNewButton_1 = new JButton("Access");
+		btnNewButton_1.setBounds(141, 93, 85, 21);
+		frame.getContentPane().add(btnNewButton_1);
+		
+		textField = new JTextField();
+		textField.setBounds(118, 29, 96, 19);
+		frame.getContentPane().add(textField);
+		textField.setColumns(10);
+		
+		textField_1 = new JTextField();
+		textField_1.setBounds(118, 52, 96, 19);
+		frame.getContentPane().add(textField_1);
+		textField_1.setColumns(10);
 	}
 }
