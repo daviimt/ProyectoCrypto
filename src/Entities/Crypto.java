4e564b27296418bf66aac90c0c5a271ca57b7440
JJFR2 <JJFR2@DESKTOP-JIDITIJ>
2022-04-26 13:05:59 +0200

    Solución errores leves.

--- src/Entities/Crypto.java
+++ src/Entities/Crypto.java
@@ -2,6 +2,63 @@ package Entities;
 
 public class Crypto {
 	private String name;
-	private float value,marketCap,supply;
-	
+
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	/**
+	 * @return the value
+	 */
+	public float getValue() {
+		return value;
+	}
+
+	/**
+	 * @param value the value to set
+	 */
+	public void setValue(float value) {
+		this.value = value;
+	}
+
+	/**
+	 * @return the marketCap
+	 */
+	public float getMarketCap() {
+		return marketCap;
+	}
+
+	/**
+	 * @param marketCap the marketCap to set
+	 */
+	public void setMarketCap(float marketCap) {
+		this.marketCap = marketCap;
+	}
+
+	/**
+	 * @return the supply
+	 */
+	public float getSupply() {
+		return supply;
+	}
+
+	/**
+	 * @param supply the supply to set
+	 */
+	public void setSupply(float supply) {
+		this.supply = supply;
+	}
+
+	private float value, marketCap, supply;
+
 }
